e17df27059289016eeee080edbb465e53403f08c
Jonathan Delves <jonathandelves@hotmail.com>
2025-08-27 14:59:13 +0100

    Clean out template - adding hero and nav components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <>
+            <nav className="max-w-6xl flex justify-center sticky p-6 space-x-5">
+                <a href="#about">Home</a>
+                <a href="#about">Projects</a>
+                <a href="#about">Skills</a>
+                <a href="#about">About/Experince</a>
+                <a href="#about">Contact</a>
+            </nav>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,13 @@
+
+const Hero = () => {
+    return (
+        <>
+            <section className="max-w-6xl p-4 flex flex-col justify-center sm:h-[50vh] md:h-[32rem]">
+                <h1 className="text-5xl pb-6 antialiased font-outfit">I'm <span> Jonathan Delves</span>, a Web Developer</h1>
+                <p className="text-base font-display">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta inventore aliquid voluptas numquam neque ullam hic aliquam veritatis dolores. Optio dolor porro enim ex eius! Fugit ducimus quae totam optio?</p>
+            </section>
+        </>
+    )
+}
+
+export default Hero
