71c9c41f0a8e1fd8d4208b1aea2c4e99db8cb377
Jonathan Delves <jonathandelves@hotmail.com>
2025-09-11 15:56:29 +0100

    Hero and page styling

--- src/components/action/action.jsx
+++ src/components/action/action.jsx
@@ -1,8 +1,8 @@
 const ActionButtons = () => {
     return(
         <div className="flex space-x-4 pt-6">
-        <a href="#" className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get in Contact</a>
-        <a href="#" className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" download>Download CV</a>
+        <a href="#" className="btn bg-[#FBD167] hover:bg-yellow-300 text-[#004FB2] font-bold py-2 px-4 rounded-3xl transition-colors duration-200">Get in Contact</a>
+        <a href="#" className="btn bg-[#FBD167] hover:bg-yellow-300 text-[#004FB2] font-bold py-2 px-4 rounded-3xl transition-colors duration-200" download>Download CV</a>
         </div>
     )
 }
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,13 +1,15 @@
 
 import ActionButtons from "../action/action"
+import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
 import "./hero.css"
 const Hero = () => {
     return (
-         <section className="hero flex justify-center">
-            <div className="max-w-6xl p-4 flex flex-col justify-center sm:h-[50vh] md:h-[32rem] ">
-                <h1 className="text-5xl pb-6 antialiased font-outfit">Hi there, I'm<span> Jonathan Delves</span>, Front-End Designer and Developer</h1>
-                <p className="text-base font-display">Hello! I’m a developer with a knack for design. I love creating beautiful websites and software, and I get a kick out of watching applications—and the companies behind them—grow. I’m up for any challenge and always willing to go the extra mile to help a project or business succeed.</p>
+         <section className="hero flex justify-center pt-20 pb-5 sm:pt-10 md:pt-0 md:pb-0">
+            <div className="max-w-6xl p-4 flex flex-col justify-center sm:h-[500px] md:h-[600px] text-white">
+                <h1 className="text-5xl pb-6 antialiased font-outfit md:w-[50%] text-[1.7rem]">Hi there, I'm<span className="block text-[3.2rem]"> Jonathan Delves</span>Front-End Designer and Developer</h1>
+                <p className="font-display lg:w-[55%] text-lg">Hello! I’m a developer with a knack for design. I love creating beautiful websites and software, and I get a kick out of watching applications—and the companies behind them—grow. I’m up for any challenge and always willing to go the extra mile to help a project or business succeed.</p>
                 <ActionButtons />
+                <div className="flex gap-4 pt-6"><FaEnvelope size={30} /> <FaLinkedin size={30} /> <FaGithub size={30} /></div>
             </div>
         </section>
     )
