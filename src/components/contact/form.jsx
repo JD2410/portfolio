ffabb6d2af4771fb48a56e94b0ac0d5b0e2efb19
Jonathan Delves <jonathandelves@hotmail.com>
2025-10-10 15:53:18 +0100

    Form error hanlding and styling

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -4,7 +4,10 @@ import './contactform.css'
 import { useState } from "react";
 
 const Form = () => {
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState({
+        label: "Send",
+        stage: null
+    });
     const [hcap, setHcap] = useState({
         token: null,
         errorMessage: null
@@ -39,7 +42,7 @@ const Form = () => {
             value: e
         }
         if(!validator.isEmail(e)) {
-            newObj.errorMessage = "Not Valid Email"
+            newObj.errorMessage = "Please enter a valid email address"
         } else {
             newObj.errorMessage = null
         }
@@ -51,7 +54,7 @@ const Form = () => {
             value: e
         }
         if (e.length <= 2) {
-            newObj.errorMessage = "Message must be more than 2 characters"
+            newObj.errorMessage = "Please enter a longer message"
         } else {
             newObj.errorMessage = null
         }
@@ -70,7 +73,7 @@ const Form = () => {
         if (hcap.token == null) { 
             setHcap({
                 ...hcap,
-                errorMessage: "Please confirm you're human 🤣"
+                errorMessage: "Please confirm you're human"
             })
             formInvalid = true
         } else {
@@ -79,6 +82,7 @@ const Form = () => {
                 errorMessage: null
             })
         }
+
         if (email.value.trim() == "") { 
             setEmail({
                 ...email,
@@ -87,6 +91,7 @@ const Form = () => {
             })
             formInvalid = true
         }
+
         if (message.value.trim() == "") { 
             setMessage({
                 ...message,
@@ -95,7 +100,7 @@ const Form = () => {
             })
             formInvalid = true
         }
-        alert(formInvalid)
+
         if(formInvalid) {
             return false;
         }
@@ -105,7 +110,10 @@ const Form = () => {
         formData.append("email", email.value);
         formData.append("h-captcha-response", hcap.token);
 
-        setResult("Sending....");
+        setResult({
+            label: 'Loading',
+            stage: 'loading'
+        });
 
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
@@ -115,12 +123,19 @@ const Form = () => {
         const data = await response.json();
 
         if (data.success) {
-            setResult("Form Submitted Successfully");
+            setResult({
+                label: 'Form Submitted Successfully',
+                stage: 'success'
+            });
             event.target.reset();
         } else {
             console.log("Error", data);
-            setResult(data.message);
+            setResult({
+                label: 'Form unable to send. Please try again',
+                stage: 'error'
+            });
         }
+        
     }
 
     return (
@@ -154,18 +169,20 @@ const Form = () => {
                     <p>{message.errorMessage}</p>
                 ) : ""}
             </div>
-            <div>
-                <HCaptcha
-                    sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
-                    reCaptchaCompat={false}
-                    onVerify={onHCaptchaChange} 
-                />
-                {hcap.errorMessage}
+            <div className={hcap.errorMessage != null ? "error" : ""}>
+                <div className='HCaptcha-container'>
+                    <HCaptcha
+                        sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+                        reCaptchaCompat={false}
+                        onVerify={onHCaptchaChange} 
+                    />
+                </div>
+                
+                <p>{hcap.errorMessage}</p>
             </div>
-            <span>{result}</span>
-            <button type="submit" className="border bg-blue-500 text-white pt-2 pb-2 rounded-md">Send</button>
-            <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
-            <span class="sr-only">Loading...</span>
+            <button type="submit" 
+                className={`border bg-blue-500 text-white pt-2 pb-2 rounded-md ${result.stage != null ? result.stage : ''}`}
+            >{result.label}</button>
         </form>
     )
 }
